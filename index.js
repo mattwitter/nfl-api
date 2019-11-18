e2d8d0192734c7840c77ddf55fca11b34b7ca078
mattwitter <matt.witter@ymail.com>
2019-11-18 12:35:57 -0500

    getting the hang of this

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const bodyParser = require('body-parser')
 const express = require('express')
+
 const teams = require('./teams.json')
 
 const app = express()
@@ -10,7 +12,7 @@ app.get('/teams', (request, response) => {
 
 app.get('/teams/:id', (request, response) => {
     let matchingTeam;
-
+    console.log(typeof request.params.id)
     if (isNaN(request.params.id)) {
         matchingTeam = teams.filter((team) => {
             return team.abbreviation === request.params.id
@@ -29,6 +31,20 @@ app.get('/teams/:id', (request, response) => {
     }
 })
 
+
+
+app.post('/teams', bodyParser.json(), (request, response) => {
+    const { id, location, mascot, abbreviation, conference, division } = request.body
+
+    if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+        response.status(400).send('The following attributes are required: id, location, mascot, abbreviation, converence, division')
+    }
+
+    const newTeam = { id, location, mascot, abbreviation, conference, division }
+
+    teams.push(newTeam)
+    response.status(201).send(newTeam)
+})
 
 const server = app.listen(1337, () => { console.log('Listening on port 1337') })
 
